9d127ad5befdf11e69a94462012639c2186b2c4a
Luddy Puerta <luddy.puerta@wiedii.co>
2022-01-15 18:41:52 -0500

    1239 - update changes

--- src/app/forum/comments/comments.component.ts
+++ src/app/forum/comments/comments.component.ts
@@ -7,21 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentsComponent implements OnInit {
 
-  isVisible:boolean = false;
-  inputPublish:string = '';
-
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  valuechange(){
-    this.isVisible = true;
-  }
-
-  publishState(){
-    this.isVisible = false;
-    this.inputPublish = '';
   }
 
 }
